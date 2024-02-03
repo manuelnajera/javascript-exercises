4f207ea4a796c0012ac0843085cb16bb07360992
Manuel Najera <30155446+manuelnajera@users.noreply.github.com>
2024-02-02 23:55:16 -0600

    Adds excercises 01 and 02 solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeatTimes) {
+    let repeatedString = "";
+    if(repeatTimes>=0){
+        for (i=1;i<=repeatTimes;i++){
+            repeatedString += string;
+    
+        };
+    }else {
+        repeatedString  =   "ERROR";
+    }
+  
+console.log(repeatedString);
+return repeatedString;
 };
 
 // Do not edit below this line
