415de4dc9a48f0b55c411dedc79dda2bc73c1633
Manuel Najera <30155446+manuelnajera@users.noreply.github.com>
2024-02-13 08:12:04 -0600

    Add exercise 05 solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    if (typeof int1 !== "number" || typeof int2 !== "number" ){
+        return "ERROR";
+    }
+    if (int1 < 0 || int2 < 0 ){
+        return "ERROR";
+    }
 
+    let maxInt;
+    let minInt;
+    if(int1>int2){
+        maxInt = int1;
+        minInt = int2;
+    }else if (int2 > int1){
+        maxInt = int2;
+        minInt= int1;
+    }else{
+        return int1 + int2;
+    }
+
+    let finalSum = 0;
+    for (let i=minInt; i <= maxInt; i++){
+        finalSum = finalSum + i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
