7be6aac89d9a85e2f4a43b4f70ed77b67e199e04
Manuel Najera <30155446+manuelnajera@users.noreply.github.com>
2024-02-13 08:56:53 -0600

    Add exercies 07 solved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,26 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitInput) {
+  let celsiusOutput;
+  if (typeof fahrenheitInput !== "number"){
+    return "ERROR";
+  }
+celsiusOutput = (fahrenheitInput -32) * (5/9);
+celsiusOutput = Number(celsiusOutput.toFixed(1).replace(/[.]00$/,""));
+
+return celsiusOutput;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusInput) {
+  let fahrenheitOutput;
+  if (typeof celsiusInput !== "number"){
+    return "ERROR";
+  }
+
+ fahrenheitOutput = (celsiusInput * (9/5)) + 32;
+ fahrenheitOutput = Number(fahrenheitOutput.toFixed(1).replace(/[.,]00$/, "")); 
+ 
+return fahrenheitOutput;
+
 };
 
 // Do not edit below this line
