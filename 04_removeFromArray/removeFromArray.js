0996332c29e542765956799c8fb5ca5dad029166
Manuel Najera <30155446+manuelnajera@users.noreply.github.com>
2024-02-13 07:41:03 -0600

    Add exercise 04 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
+let modifiedArray;
 
+const removeFromArray = function(originalArray = [], ...argsRemoved) {
+  console.log("Original array", originalArray);
+  modifiedArray = originalArray;
+   
+     for (arg of argsRemoved){
+       removeItems(modifiedArray,arg);
+     
+       console.log("Arg to delete:",arg,"modified Array",modifiedArray)
+     }
+
+     console.log("Resultado:", modifiedArray);
+     return modifiedArray;
 };
+
+function removeItems(arr, arg){
+    modifiedArray = arr.filter((el) => el !== arg);
+    console.log("removeItemsTest:" ,modifiedArray)
+    return modifiedArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
